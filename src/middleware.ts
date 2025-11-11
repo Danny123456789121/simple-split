92abaf4f38065ff838589cfe79895e201d1219c9
Daniel Kurth <kurth_d@icloud.com>
2025-11-11 23:14:25 +0100

    add arcjet bot detection & rate limiting

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,31 @@
+import arcjet, { detectBot, shield, slidingWindow } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+import { env } from './data/env/server';
+
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/']);
+
+arcjet({
+  key: env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:MONITOR', 'CATEGORY:PREVIEW'],
+    }),
+    slidingWindow({
+      mode: 'LIVE',
+      interval: '1m',
+      max: 100,
+    }),
+  ],
+});
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
 });
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
